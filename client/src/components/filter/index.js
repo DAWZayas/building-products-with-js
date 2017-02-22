f05398b3065be1a8ef9dbde9013c2a86375ae3d9
surtich <surtich@gmail.com>
2017-02-22 11:00:56 +0100

    [client] Added search question component

---
+++ client/src/components/filter/index.js
@@ -0,0 +1,11 @@
+// npm packages
+import React from 'react';
+
+export default () => (
+  <div className="input-group">
+    <span className="input-group-addon">
+      <span className="glyphicon glyphicon-search" />
+    </span>
+    <input type="text" className="form-control" placeholder="Search" />
+  </div>
+);
--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -4,18 +4,26 @@ import MediaQuery from 'react-responsive';
 
 // our packages
 import {QuestionList, QuestionSingle} from '../../components/question';
+import Filter from '../../components/filter';
 
 const Home = () => (
   <div className="container">
-    <MediaQuery query="(min-width: 992px)">
-      {(matches) => {
-        if (matches) {
-          return <QuestionList />;
-        } else {
-          return <QuestionSingle />;
-        }
-      }}
-    </MediaQuery>
+    <div className="row">
+      <div className="col-xs-4 pull-right">
+        <Filter />
+      </div>
+    </div>
+    <div className="row" style={{marginTop: '20px'}}>
+      <MediaQuery query="(min-width: 992px)">
+        {(matches) => {
+          if (matches) {
+            return <QuestionList />;
+          } else {
+            return <QuestionSingle />;
+          }
+        }}
+      </MediaQuery>
+    </div>
   </div>
 );
 
